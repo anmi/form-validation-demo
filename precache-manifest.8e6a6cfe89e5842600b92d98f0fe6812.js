86350836adf70cd01728c06b424645a79ced8a02
Andrei Miroshnik <anmi.asm@gmail.com>
2020-01-13 23:37:52 +0300

    Updates

---
+++ precache-manifest.8e6a6cfe89e5842600b92d98f0fe6812.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e70c4042bff82be158b7cd673172995e",
+    "url": "/form-validation-demo/index.html"
+  },
+  {
+    "revision": "29f8624c35654fd70b5d",
+    "url": "/form-validation-demo/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "cfdf7dd3ba74ffa0d91f",
+    "url": "/form-validation-demo/static/css/main.54c18f54.chunk.css"
+  },
+  {
+    "revision": "29f8624c35654fd70b5d",
+    "url": "/form-validation-demo/static/js/2.c9bb8212.chunk.js"
+  },
+  {
+    "revision": "7b30fc68c3fb89d4e4dc4c75fdaf536d",
+    "url": "/form-validation-demo/static/js/2.c9bb8212.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cfdf7dd3ba74ffa0d91f",
+    "url": "/form-validation-demo/static/js/main.2a53c55b.chunk.js"
+  },
+  {
+    "revision": "2a56e7ef2e14a0f2e68a",
+    "url": "/form-validation-demo/static/js/runtime-main.6d5d87df.js"
+  }
+]);
